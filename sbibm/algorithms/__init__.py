64e86f0be610a96e14a2bfdcf720028b1cb455ed
Dongxia Wu <36138851+DongxiaW@users.noreply.github.com>
2022-05-02 22:56:32 -0700

    Update __init__.py

--- sbibm/algorithms/__init__.py
+++ sbibm/algorithms/__init__.py
@@ -3,6 +3,7 @@ from sbibm.algorithms.sbi.smcabc import run as smcabc
 from sbibm.algorithms.sbi.snle import run as snle
 from sbibm.algorithms.sbi.snpe import run as snpe
 from sbibm.algorithms.sbi.snre import run as snre
+from sbibm.algorithms.sbi.boed import run as boed
 
 rej_abc = mcabc
 smc_abc = smcabc
